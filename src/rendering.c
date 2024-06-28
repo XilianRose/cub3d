7ee33afed1d6e32dbefc7c4155cc3ec6fe666827
Michelle <m.stegeman@me.com>
2024-06-28 19:54:55 +0200

    split minimap render function to norm and adjust calculations

--- src/rendering.c
+++ src/rendering.c
@@ -6,19 +6,36 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/28 11:14:01 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/06/28 15:35:00 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/06/28 19:51:40 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	put_minimap(mlx_image_t *minimap, u_int32_t x, u_int32_t y, u_int32_t color)
+{
+	int			yy;
+	int			xx;
+
+	yy = 0;
+	while (yy < TILE_SIZE)
+	{
+		xx = 0;
+		while (xx < TILE_SIZE)
+		{
+			mlx_put_pixel(minimap, x * TILE_SIZE + xx, y * TILE_SIZE + yy, color);
+			xx++;
+		}
+		yy++;
+	}
+	return ;
+}
 
 int32_t	render_minimap(t_game_info *game)
 {
 	t_map_info	map;
 	int			y;
 	int			x;
-	int			yy;
-	int			xx;
 
 	y = 0;
 	map = game->map;
@@ -29,39 +46,14 @@ int32_t	render_minimap(t_game_info *game)
 		while (map.layout[y][x] != '\0')
 		{
 			if (map.layout[y][x] == '1')
-			{
-				yy = 0;
-				while (yy < TILE_SIZE)
-				{
-					xx = 0;
-					while (xx < TILE_SIZE)
-					{
-						mlx_put_pixel(map.minimap, x + xx, y + yy, 0xFFFFFFFF);
-						xx++;
-					}
-					yy++;
-				}
-			}
-			else if (map.layout[y][x] == '0')
-			{
-				yy = 0;
-				while (yy < TILE_SIZE)
-				{
-					xx = 0;
-					while (xx < TILE_SIZE)
-					{
-						mlx_put_pixel(map.minimap, x + xx, y + yy, 0xFFFFFF80);
-						xx++;
-					}
-					yy++;
-				}
-			}
+				put_minimap(map.minimap, x, y, 0xFFFFFFCC);
+			else if (ft_strchr("0NOSW",map.layout[y][x]) != NULL)
+				put_minimap(map.minimap, x, y, 0xFFFFFF80);
 			x++;
 		}
 		y++;
 	}
-	if (mlx_image_to_window(game->mlx, map.minimap, \
-	10, 10) == -1)
+	if (mlx_image_to_window(game->mlx, map.minimap, 10, 10) == -1)
 		return (mlx_error_wrapper(game->mlx), EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
