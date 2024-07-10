050234268028477ab9ddc1229b81ee48d03c29ee
mstegema <mstegema@student.codam.nl>
2024-07-10 14:52:39 +0200

    implement playermovement
    issue when running into wall

--- src/player_movement.c
+++ src/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/29 16:08:57 by mstegema      #+#    #+#                 */
-/*   Updated: 2024/07/05 15:15:43 by mstegema      ########   odam.nl         */
+/*   Updated: 2024/07/10 13:57:39 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,74 @@
 
 //calculate movement based on time
 
-#define MOVESPEED 0.6
-
 void	move_up(t_game_info *game)
 {
 	t_player_info	*player;
+	double			movespeed;
 	int				x;
 	int				y;
 
 	player = &game->player;
+	movespeed = game->time * 3.0;
 	x = (int) player->position.x;
 	y = (int) player->position.y;
-	if (game->map.layout[y][x + (int)(player->dir.x * MOVESPEED)] != '1')
-		player->position.x += (int)(player->dir.x * MOVESPEED);
-	if (game->map.layout[y + (int)(player->dir.y * MOVESPEED)][x] != '1')
-		player->position.y += (int)(player->dir.y * MOVESPEED);
+	if (game->map.layout[y][x + (int)(player->dir.x * movespeed)] != '1')
+		player->position.x += player->dir.x * movespeed;
+	if (game->map.layout[y + (int)(player->dir.y * movespeed)][x] != '1')
+		player->position.y += player->dir.y * movespeed;
 	return ;
 }
 
 void	move_down(t_game_info *game)
 {
 	t_player_info	*player;
+	double			movespeed;
 	int				x;
 	int				y;
 
 	player = &game->player;
+	movespeed = game->time * 3.0;
 	x = (int) player->position.x;
 	y = (int) player->position.y;
-	if (game->map.layout[y][x - (int)(player->dir.x * MOVESPEED)] != '1')
-		player->position.x -= (int)(player->dir.x * MOVESPEED);
-	if (game->map.layout[y - (int)(player->dir.y * MOVESPEED)][x] != '1')
-		player->position.y -= (int)(player->dir.y * MOVESPEED);
+	if (game->map.layout[y][x - (int)(player->dir.x * movespeed)] != '1')
+		player->position.x -= player->dir.x * movespeed;
+	if (game->map.layout[y - (int)(player->dir.y * movespeed)][x] != '1')
+		player->position.y -= player->dir.y * movespeed;
 	return ;
 }
 
 void	move_left(t_game_info *game)
 {
-	int	x;
-	int	y;
+	t_player_info	*player;
+	double			movespeed;
+	int				x;
+	int				y;
 
-	x = (game->player.image->instances[0].x - 10) / game->map.ratio;
-	y = (game->player.image->instances[0].y - 10) / game->map.ratio;
-	if (game->map.layout[y][x - 1] != '1')
-		game->player.image->instances[0].x -= game->map.ratio;
+	player = &game->player;
+	movespeed = game->time * 3.0;
+	x = (int) player->position.x;
+	y = (int) player->position.y;
+	if (game->map.layout[y][x - (int)(player->dir.x * movespeed)] != '1')
+		player->position.x -= player->ray.plane.x * movespeed;
+	if (game->map.layout[y - (int)(player->dir.y * movespeed)][x] != '1')
+		player->position.y -= player->ray.plane.y * movespeed;
 	return ;
 }
 
 void	move_right(t_game_info *game)
 {
-	int	x;
-	int	y;
+	t_player_info	*player;
+	double			movespeed;
+	int				x;
+	int				y;
 
-	x = (game->player.image->instances[0].x - 10) / game->map.ratio;
-	y = (game->player.image->instances[0].y - 10) / game->map.ratio;
-	if (game->map.layout[y][x + 1] != '1')
-		game->player.image->instances[0].x += game->map.ratio;
+	player = &game->player;
+	movespeed = game->time * 3.0;
+	x = (int) player->position.x;
+	y = (int) player->position.y;
+	if (game->map.layout[y][x + (int)(player->dir.x * movespeed)] != '1')
+		player->position.x += player->ray.plane.x * movespeed;
+	if (game->map.layout[y + (int)(player->dir.y * movespeed)][x] != '1')
+		player->position.y += player->ray.plane.y * movespeed;
 	return ;
 }
